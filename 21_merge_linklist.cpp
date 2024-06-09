cee0e54085d5fe67ed50db6d57600a2334e153de
krish <sharmakrish1810work@gmail.com>
2024-06-09 18:48:13 +0530

    updated question 21

--- 21_merge_linklist.cpp
+++ 21_merge_linklist.cpp
@@ -21,8 +21,46 @@ public:
         this->val = x;
         this->next = next;
     }
+
+    void InsertTail(int value, ListNode *&tail)
+    {
+        // creating a temp node
+        ListNode *temp = new ListNode(value);
+
+        tail->next = temp;
+        tail = tail->next;
+    }
+
     ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
     {
+        ListNode *list3 = new ListNode();
+        ListNode *tail = list3;
+        while (list1 != NULL && list2 != NULL)
+        {
+            if (list1->val >= list2->val)
+            {
+                InsertTail(list2->val, tail);
+                list2 = list2->next;
+            }
+            else
+            {
+                InsertTail(list1->val, tail);
+                list1 = list1->next;
+            }
+        }
+        while (list1 != NULL)
+        {
+            InsertTail(list1->val, tail);
+            list1 = list1->next;
+        }
+        while (list2 != NULL)
+        {
+            InsertTail(list2->val, tail);
+            list2 = list2->next;
+        }
+
+        list3 = list3->next;
+        return list3;
     }
 };
 
@@ -51,18 +89,23 @@ void PrintList(ListNode *&head)
 
 int main()
 {
+    ListNode obj;
+
     ListNode *list1 = new ListNode(1);
     ListNode *tail1 = list1;
     ListNode *head1 = list1;
-    InsertTail(2, tail1);
-    InsertTail(4, tail1);
+    obj.InsertTail(2, tail1);
+    obj.InsertTail(4, tail1);
     PrintList(head1);
 
     ListNode *list2 = new ListNode(1);
     ListNode *tail2 = list2;
     ListNode *head2 = list2;
-    InsertTail(3, tail2);
-    InsertTail(4, tail2);
+    obj.InsertTail(3, tail2);
+    obj.InsertTail(4, tail2);
     PrintList(head2);
+
+    ListNode *list3 = obj.mergeTwoLists(head1, head2);
+    PrintList(list3);
     return 0;
 }
